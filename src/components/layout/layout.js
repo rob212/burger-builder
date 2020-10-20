68aff5a6a5ec95ad2d76ba93e280ece773bc1d96
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-10-20 16:51:25 +0100

    initial commit

---
+++ src/components/layout/layout.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const layout = (props) => (
+  <Fragment>
+    <div>Toolbar, Sidebar, Backdrop</div>
+    <main>{props.children}</main>
+  </Fragment>
+);
+
+export default layout;
