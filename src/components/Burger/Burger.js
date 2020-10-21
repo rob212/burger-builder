69f136bc853cd0757959e7479bbad7c5de9f5f0c
Rob_McBryde <robert.mcbryde@ros.gov.uk>
2020-10-21 11:38:28 +0100

    addition state to hold ingredients and logic to display graphics based on the ingredient state.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-// import styles from './Burger.modules.css';
+import styles from "./Burger.module.css";
 
-const burger = () => (
-    <BurgerIngredient type='bread-bottom'>yum</BurgerIngredient>
-)
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please enter some tasty ingredients</p>;
+  }
+
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type="bread-top"></BurgerIngredient>
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+    </div>
+  );
+};
 
 export default burger;
